288168a165a55118419046de6dd8f05a89f743d5
Tan Samuel Susanto <tan@atech.software>
2023-11-10 19:35:42 +0700

    Update mail_send.php

--- smtpmail/mail_send.php
+++ smtpmail/mail_send.php
@@ -17,25 +17,15 @@
         $mail = new PHPMailer(true);
 
             try {
-                // //Server settings
-                // $mail->SMTPDebug = 3;  //Enable verbose debug output
-                // $mail->isSMTP(true);   //Send using SMTP
-                // $mail->Host       = 'email-smtp.ap-southeast-1.amazonaws.com'; //hostname/domain yang dipergunakan untuk setting smtp
-                // $mail->SMTPAuth   = true;  //Enable SMTP authentication
-                // $mail->Username   = 'AKIAUQUCCF6GSJDVC3L5'; //SMTP username
-                // $mail->Password   = 'BMFZlACWibjXSlH2MH2Xg80iZVSxNNvt3MqXeCF2d+sx';   //SMTP password
-                // // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;   //Enable implicit TLS encryption
-                // $mail->SMTPSecure = "tls";
-                // $mail->Port       = 587;   //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
                 //Server settings
-                $mail->SMTPDebug = SMTP::DEBUG_SERVER;  //Enable verbose debug output
+                // $mail->SMTPDebug = SMTP::DEBUG_SERVER;  //Enable verbose debug output
+                $mail->SMTPDebug = 3;  //Enable verbose debug output
                 $mail->isSMTP(true);   //Send using SMTP
                 $mail->Host       = 'email-smtp.ap-southeast-1.amazonaws.com'; //hostname/domain yang dipergunakan untuk setting smtp
                 $mail->SMTPAuth   = true;  //Enable SMTP authentication
                 $mail->Username   = 'AKIAUQUCCF6GSJDVC3L5'; //SMTP username
                 $mail->Password   = 'BMFZlACWibjXSlH2MH2Xg80iZVSxNNvt3MqXeCF2d+sx';   //SMTP password
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;   //Enable implicit TLS encryption
+                // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;   //Enable implicit TLS encryption
                 $mail->SMTPSecure = "tls";
                 $mail->Port       = 587;   //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
@@ -60,56 +50,56 @@
                 // echo 'Message has been sent';
 
                 if(isset($_POST['formtype'])=="signup"){    
-                        $to = "info@spslogistics.io"; // this is your Email address
-                        $from = $_POST['email']; // this is the sender's Email address
-                        // $first_name = $_POST['first_name'];
-                        // $last_name = $_POST['last_name'];
-                        $subject = "Form Sign Up Spslogistics.io";
-                        $subject2 = "Copy of Your Form Sign Up Spslogistics.io";
-                        // $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-                        // $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-                        $message = "New Sign Up user";
-                        $message2 = "Thank you for your sign up.";
-                        $headers = "From:" . $from;
-                        $headers2 = "From:" . $to;
-                        // mail($to,$subject,$message,$headers);
-                        // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-                        // echo "Mail Sent. Thank you " . $from . ", we will contact you shortly.";
-                        
-                        // mail1
-                        //Recipients
-                        $mail->setFrom($from, $from);
-                        $mail->addAddress($to, $to);
-                        $mail->addReplyTo('tan@atech.software', 'Information'); 
-                        $mail->addCC('tan@atech.software');
-                        $mail->addBCC('tan@atech.software');
-                        //Content
-                        $mail->isHTML(true);   //Set email format to HTML
-                        $mail->Subject = $subject;
-                        $mail->Body    = $message;
-                        $mail->AltBody = $message;
-                        $mail->send();
+                    $to = "info@spslogistics.io"; // this is your Email address
+                    $from = $_POST['email']; // this is the sender's Email address
+                    // $first_name = $_POST['first_name'];
+                    // $last_name = $_POST['last_name'];
+                    $subject = "Form Sign Up Spslogistics.io";
+                    $subject2 = "Copy of Your Form Sign Up Spslogistics.io";
+                    // $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+                    // $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+                    $message = "New Sign Up user";
+                    $message2 = "Thank you for your sign up.";
+                    $headers = "From:" . $from;
+                    $headers2 = "From:" . $to;
+                    // mail($to,$subject,$message,$headers);
+                    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+                    // echo "Mail Sent. Thank you " . $from . ", we will contact you shortly.";
+                    
+                    // mail1
+                    //Recipients
+                    $mail->setFrom($from, $from);
+                    $mail->addAddress($to, $to);
+                    $mail->addReplyTo('tan@atech.software', 'Information'); 
+                    $mail->addCC('tan@atech.software');
+                    $mail->addBCC('tan@atech.software');
+                    //Content
+                    $mail->isHTML(true);   //Set email format to HTML
+                    $mail->Subject = $subject;
+                    $mail->Body    = $message;
+                    $mail->AltBody = $message;
+                    $mail->send();
 
-                        // mail2
-                        //Recipients
-                        $mail2->setFrom($to, $to);
-                        $mail2->addAddress($from, $from);
-                        $mail2->addReplyTo('tan@atech.software', 'Information'); 
-                        $mail2->addCC('tan@atech.software');
-                        $mail2->addBCC('tan@atech.software');
-                        //Content
-                        $mail2->isHTML(true);   //Set email format to HTML
-                        $mail2->Subject = $subject2;
-                        $mail2->Body    = $message2;
-                        $mail2->AltBody = $message2;
-                        $mail2->send();
-                        header('Location: index.php?message=Signup Success. Thank you ' . $from . ', we will contact you shortly.');
-                        
-                    }
+                    // mail2
+                    //Recipients
+                    $mail2->setFrom($to, $to);
+                    $mail2->addAddress($from, $from);
+                    $mail2->addReplyTo('tan@atech.software', 'Information'); 
+                    $mail2->addCC('tan@atech.software');
+                    $mail2->addBCC('tan@atech.software');
+                    //Content
+                    $mail2->isHTML(true);   //Set email format to HTML
+                    $mail2->Subject = $subject2;
+                    $mail2->Body    = $message2;
+                    $mail2->AltBody = $message2;
+                    $mail2->send();
+                    header('Location: index.php?message=Signup Success. Thank you ' . $from . ', we will contact you shortly.');
+                }   
 
 
             } catch (Exception $e) {
-                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                header('Message could not be sent. Mailer Error: '. $mail->ErrorInfo);
             }
     }        
 ?>
