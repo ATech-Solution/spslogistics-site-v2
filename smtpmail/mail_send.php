225453fa5f0d6e6636200ee9c83c67aa8629728c
Tan Samuel Susanto <tan@atech.software>
2023-11-11 01:18:29 +0700

    Update mail_send.php

--- smtpmail/mail_send.php
+++ smtpmail/mail_send.php
@@ -28,53 +28,54 @@
                 $mail->SMTPSecure = "tls";
                 $mail->Port       = 587;   //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
+                if(isset($_POST['formtype'])=="signup"){    
+                    $to = "info@spslogistics.io"; // this is your Email address
+                    $from = $_POST['email']; // this is the sender's Email address
+                    // $first_name = $_POST['first_name'];
+                    // $last_name = $_POST['last_name'];
+                    $subject = "Form Sign Up Spslogistics.io";
+                    $subject2 = "Copy of Your Form Sign Up Spslogistics.io";
+                    // $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+                    // $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+                    $message = "New Sign Up user";
+                    $message2 = "Thank you for your sign up.";
+                    $headers = "From:" . $from;
+                    $headers2 = "From:" . $to;
+                    // mail($to,$subject,$message,$headers);
+                    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+                    // echo "Mail Sent. Thank you " . $from . ", we will contact you shortly.";
+                    // header('Location: index.php?message=Signup Success. Thank you ' . $from . ', we will contact you shortly.');
+                    // You can also use header('Location: thank_you.php'); to redirect to another page.
+                    // You cannot use header and echo together. It's one or the other.
 
-                $to = "info@spslogistics.io"; // this is your Email address
-                $from = $_POST['email']; // this is the sender's Email address
-                // $first_name = $_POST['first_name'];
-                // $last_name = $_POST['last_name'];
-                $subject = "Form Sign Up Spslogistics.io";
-                $subject2 = "Copy of Your Form Sign Up Spslogistics.io";
-                // $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-                // $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-                $message = "New Sign Up user";
-                $message2 = "Thank you for your sign up.";
-                $headers = "From:" . $from;
-                $headers2 = "From:" . $to;
-                // mail($to,$subject,$message,$headers);
-                // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-                // echo "Mail Sent. Thank you " . $from . ", we will contact you shortly.";
-                // header('Location: index.php?message=Signup Success. Thank you ' . $from . ', we will contact you shortly.');
-                // You can also use header('Location: thank_you.php'); to redirect to another page.
-                // You cannot use header and echo together. It's one or the other.
+                    //Recipients
+                    $mail->setFrom('info@spslogistics.io', 'Info SPS');
+                    $mail->addAddress($from, $from);     //email tujuan
+                    // $mail->addReplyTo('tan@atech.software', 'Information'); //email tujuan add reply (bila tidak dibutuhkan bisa diberi pagar)
+                    // $mail->addCC('tan@atech.software'); // email cc (bila tidak dibutuhkan bisa diberi pagar)
+                    // $mail->addBCC('tan@atech.software'); // email bcc (bila tidak dibutuhkan bisa diberi pagar)
+                    //Attachments
+                    #$mail->addAttachment('/var/tmp/file.tar.gz');   //Add attachments
+                    #$mail->addAttachment('/tmp/image.jpg', 'new.jpg');  //Optional name
+                    //Content
+                    $mail->isHTML(true);   //Set email format to HTML
+                    $mail->Subject = $subject;
+                    $mail->Body    = $message;
+                    $mail->AltBody = $message;
+                    $mail->send();
 
-                //Recipients
-                $mail->setFrom('info@spslogistics.io', 'Info SPS');
-                $mail->addAddress($from, $from);     //email tujuan
-                // $mail->addReplyTo('tan@atech.software', 'Information'); //email tujuan add reply (bila tidak dibutuhkan bisa diberi pagar)
-                // $mail->addCC('tan@atech.software'); // email cc (bila tidak dibutuhkan bisa diberi pagar)
-                // $mail->addBCC('tan@atech.software'); // email bcc (bila tidak dibutuhkan bisa diberi pagar)
-                //Attachments
-                #$mail->addAttachment('/var/tmp/file.tar.gz');   //Add attachments
-                #$mail->addAttachment('/tmp/image.jpg', 'new.jpg');  //Optional name
-                //Content
-                $mail->isHTML(true);   //Set email format to HTML
-                $mail->Subject = $subject;
-                $mail->Body    = $message;
-                $mail->AltBody = $message;
-                $mail->send();
-
-                $mail->setFrom('info@spslogistics.io', 'Info SPS');
-                $mail->addAddress($from, $from);     //email tujuan
-                $mail->isHTML(true);   //Set email format to HTML
-                $mail->Subject = $subject2;
-                $mail->Body    = $message2;
-                $mail->AltBody = $message2;
-                $mail->send();
-                header('Location: ../index.php?message=Signup Success. Thank you ' . $from . ', we will contact you shortly.');
-                // echo 'Message has been sent';
+                    $mail->setFrom('info@spslogistics.io', 'Info SPS');
+                    $mail->addAddress($from, $from);     //email tujuan
+                    $mail->isHTML(true);   //Set email format to HTML
+                    $mail->Subject = $subject2;
+                    $mail->Body    = $message2;
+                    $mail->AltBody = $message2;
+                    $mail->send();
+                    header('Location: ./index.php?message=Signup Success. Thank you ' . $from . ', we will contact you shortly.');
+                    // echo 'Message has been sent';
+                }
             } catch (Exception $e) {
-                header('Location: ../index.php?message=Message could not be sent. Mailer Error: '. $mail->ErrorInfo .'. ');
+                header('Location: ./index.php?message=Message could not be sent. Mailer Error: '. $mail->ErrorInfo .'. ');
                 // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
             }
 ?>
