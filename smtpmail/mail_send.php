7ee121af88c2dfb924426fe04b45e042ed97bd1d
Tan Samuel Susanto <tan@atech.software>
2023-11-11 02:29:35 +0700

    Update mail_send.php

--- smtpmail/mail_send.php
+++ smtpmail/mail_send.php
@@ -30,6 +30,63 @@
                 $mail->SMTPSecure = "tls";
                 $mail->Port       = 587;   //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
+                if(isset($_POST['formtype'])=="contact"){
+                        $to = "info@spslogistics.io"; // this is your Email address
+                        $from = $_POST['your-email']; // this is the sender's Email address
+                        $subject = "Form Contact Spslogistics.io";
+                        $subject2 = "Your Form Contact Spslogistics.io";
+                        $name ="";
+                        $email ="";
+                        $number ="";
+                        $company ="";
+                        $interest ="";
+                        $message ="";
+                        $interests = "";
+                        if (isset($_POST['your-name'])) {
+                            $name = $_POST['your-name'];
+                        }else if (isset($_POST['your-email'])) {
+                            $email = $_POST['your-email'];
+                        }else if (isset($_POST['your-number'])) {
+                            $number = $_POST['your-number'];
+                        }else if (isset($_POST['your-company'])) {
+                            $company = $_POST['your-company'];
+                        }else if (isset($_POST['interested'])) {
+                            $interest = $_POST['interested'];
+                            if (isset($_POST['interested'])) {
+                                foreach ($interest as $interested){ 
+                                    $interests .= $interested .",";
+                                }
+                            }
+                        }else if (isset($_POST['your-name'])) {
+                            $message = $_POST['your-message'];
+                        }
+                        $messages = "Name : ". $name . "<br>Email : ". $email . "<br>Phone Number : ". $number . "<br>Company : ". $company . "<br>Interest : ". $interests . "<br>message ". $message;
+                        $message2 = "Thank you for your contact, we will contact you shortly.";
+                        // $headers = "From:" . $from;
+                        // $headers2 = "From:" . $to;
+
+                        //Recipients
+                        $mail->setFrom('info@spslogistics.io', 'Info SPS');
+                        $mail->addAddress($from, $from);
+                        $mail->addReplyTo('info@spslogistics.io', 'Information'); //email tujuan add reply (bila tidak dibutuhkan bisa 
+                        $mail->isHTML(true);
+                        $mail->Subject = $subject;
+                        $mail->Body    = $messages;
+                        $mail->AltBody = $messages;
+                        $mail->send();
+
+                        $mail->setFrom('info@spslogistics.io', 'Info SPS');
+                        $mail->addAddress($from, $from);     //email tujuan
+                        $mail->isHTML(true);   //Set email format to HTML
+                        $mail->Subject = $subject2;
+                        $mail->Body    = $message2;
+                        $mail->AltBody = $message2;
+                        $mail->send();
+                        header('Location: ../../index.php?message=Submit Contact Success. Thank you ' . $name . ', we will contact you shortly.');
+                    }   
+
+
+
                 if(isset($_POST['formtype'])=="signup"){    
                     $to = "info@spslogistics.io"; // this is your Email address
                     $from = $_POST['email']; // this is the sender's Email address
@@ -76,60 +133,6 @@
                     header('Location: ../../index.php?message=Signup Success. Thank you ' . $from . ', we will contact you shortly.');
 
                 } 
-                if(isset($_POST['formtype'])=="contact"){
-                        $to = "info@spslogistics.io"; // this is your Email address
-                        $from = $_POST['your-email']; // this is the sender's Email address
-                        $subject = "Form Contact Spslogistics.io";
-                        $subject2 = "Your Form Contact Spslogistics.io";
-                        $name ="";
-                        $email ="";
-                        $number ="";
-                        $company ="";
-                        $interest ="";
-                        $message ="";
-                        $interests = "";
-                        if (isset($_POST['your-name'])) {
-                            $name = $_POST['your-name'];
-                        }else if (isset($_POST['your-email'])) {
-                            $email = $_POST['your-email'];
-                        }else if (isset($_POST['your-number'])) {
-                            $number = $_POST['your-number'];
-                        }else if (isset($_POST['your-company'])) {
-                            $company = $_POST['your-company'];
-                        }else if (isset($_POST['interested'])) {
-                            $interest = $_POST['interested'];
-                            if (isset($_POST['interested'])) {
-                                foreach ($interest as $interested){ 
-                                    $interests .= $interested .",";
-                                }
-                            }
-                        }else if (isset($_POST['your-name'])) {
-                            $message = $_POST['your-message'];
-                        }
-                        $messages = "Name : ". $name . "<br>Email : ". $email . "<br>Phone Number : ". $number . "<br>Company : ". $company . "<br>Interest : ". $interests . "<br>message ". $message;
-                        $message2 = "Thank you for your contact, we will contact you shortly.";
-                        // $headers = "From:" . $from;
-                        // $headers2 = "From:" . $to;
-
-                        //Recipients
-                        $mail->setFrom('info@spslogistics.io', 'Info SPS');
-                        $mail->addAddress($from, "SPS email");
-                        $mail->isHTML(true);
-                        $mail->Subject = $subject;
-                        $mail->Body    = $messages;
-                        $mail->AltBody = $messages;
-                        $mail->send();
-
-                        $mail->setFrom('info@spslogistics.io', 'Info SPS');
-                        $mail->addAddress($from, "Your email");     //email tujuan
-                        $mail->isHTML(true);   //Set email format to HTML
-                        $mail->Subject = $subject2;
-                        $mail->Body    = $message2;
-                        $mail->AltBody = $message2;
-                        $mail->send();
-                        header('Location: ../../index.php?message=Submit Contact Success. Thank you ' . $name . ', we will contact you shortly.');
-                    }   
-
             // } catch (Exception $e) {
             //     header('Location: ../../index.php?message=Message could not be sent. Mailer Error: '. $mail->ErrorInfo .'. ');
             //     // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
