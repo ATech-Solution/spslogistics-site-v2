3173d4b6abc4c3cc7f0d050430e4223eb1fd81c0
Tan Samuel Susanto <tan@atech.software>
2023-11-20 14:29:22 +0700

    disable grecap

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">	 -->
 	<link href="assets/css/style.css" rel="stylesheet">
 	<link href="assets/css/custom-css.css" rel="stylesheet">
-	<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+	<!-- <script src="https://www.google.com/recaptcha/api.js" async defer></script> -->
 	<script>
 		// security     
 	// disable copy paste html
