ef03a4ec5d355308d0529c81336168d575ccfdcf
tansamuel <tan@atech.software>
2023-12-19 19:50:02 +0700

    create quotation.php

---
+++ quotation.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+	<!-- Document Meta
+    ============================================= -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!--IE Compatibility Meta-->
+	<meta name="author" content="zytheme"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="description" content="Multi-purpose Business html5 landing page">
+	<link href="assets/img/favicon/faveicon.png" rel="icon">
+
+	<!-- Fonts
+    ============================================= -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i%7CMerriweather:300,300i,400,400i,700,700i,900,900i' rel='stylesheet' type='text/css'>
+
+	<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+	<!-- Stylesheets
+    ============================================= -->
+	<link href="assets/css/external.css" rel="stylesheet">
+	<link href="assets/css/bootstrap.min_.css" rel="stylesheet">
+	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">	 -->
+	<link href="assets/css/style.css" rel="stylesheet">
+	<link href="assets/css/custom-css.css" rel="stylesheet">
+	<!-- <script src="https://www.google.com/recaptcha/api.js" async defer></script> -->
+	<script>
+		// security     
+	// disable copy paste html
+	window.onload = () => {
+	  const myInput = document.getElementsByClassName("inputClass");
+	  myInput.onpaste = e => e.preventDefault();
+	}
+
+	// // google recaptcha
+	// function onSubmit(token) {
+	//   // alert('thanks ' + document.getElementById('field').value);
+	//   // alert('grecaptcha')
+	//   document.getElementById("signUpForm").submit();
+	// }
+	// // google recaptcha
+	// function onSubmitContact(token) {
+	//   document.getElementById("contactForm").submit();
+	// }
+	// function validate(event) {
+	//   event.preventDefault();
+	//   // if (!document.getElementById('field').value) {
+	//   //   alert("You must add text to the required field");
+	//   // } else {
+	//     grecaptcha.execute();
+	//   // }
+	// }
+	// function onloads() {
+	//   var element = document.getElementById('submit');
+	//   element.onclick = validate;
+	// }
+	// function onloadsContact() {
+	//   var element = document.getElementById('submit');
+	//   element.onclick = validate;
+	// }
+	
+	</script>
+	<!--Start of Tawk.to Script-->
+	<script type="text/javascript">
+		var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+		(function(){
+		var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+		s1.async=true;
+		s1.src='https://embed.tawk.to/6549e70ba84dd54dc48944ae/1hekat293';
+		s1.charset='UTF-8';
+		s1.setAttribute('crossorigin','*');
+		s0.parentNode.insertBefore(s1,s0);
+		})();
+	</script>
+	<!--End of Tawk.to Script-->
+	<!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+	<!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+      <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+
+	<!-- Document Title
+    ============================================= -->
+	<title>SPS | End To End Supply Chain & Logistics Solution</title>
+
+	<!-- Google tag (gtag.js) -->
+	<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11209400994"></script> -->
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'AW-11209400994');
+	</script>
+</head>
+
+<body class="body-scroll home">
+<!-- Document Wrapper
+============================================= -->
+<div id="wrapper" class="wrapper clearfix">	<!-- Header
+        ============================================= -->
+
+
+	<!-- Footer #5
+	============================================= -->
+	<footer id="footer" class="footer section">
+			<!-- Widget Section
+		============================================= -->
+		<div class="footer-widget cta">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-7 pr-60 pl-60 mr-60 connectSection">
+						<div class="heading heading-1 wow fadeInUp" data-wow-duration="1s">
+							<h2 class="heading--title yellow-light-heading-footer mb-60">Connect with our expert</h2>
+						</div>
+						<div class="form-request">
+							<div class="row"><!-- mail_handler_contact.php -->
+								<!-- <form action="#" method="post" class="wow fadeInUp mb-0" data-wow-duration="1s">	 -->
+								<form id="contactForm" action="smtpmail/mail_send_contact.php" method="post" class="wow fadeInUp mb-0" data-wow-duration="1s">	
+								<!-- <form action="mail_handler_contact.php" method="post" class="wow fadeInUp mb-0" data-wow-duration="1s"> -->
+									<div class="col-xs-12 col-sm-12 col-md-5 mr-30">
+										<label class="form-label">Name*</label>
+										<input type="text" class="form-control inputClass" name="your-name" placeholder="Your Name" required>
+									</div>
+									<!-- .col-md-6 end -->
+									<div class="col-xs-12 col-sm-12 col-md-5 ml-50">
+										<label class="form-label">Email*</label>
+										<input type="email" class="form-control inputClass" name="your-email" placeholder="Your Email" required>
+									</div>
+									<!-- .col-md-6 end -->
+									<div class="col-xs-12 col-sm-12 col-md-5 mr-30">
+										<label class="form-label">Phone</label>
+										<input type="number" class="form-control inputClass" name="your-number" placeholder="Your Phone number" maxlength="9">
+									</div>
+									<div class="col-xs-12 col-sm-12 col-md-5 ml-50">
+										<label class="form-label">Company</label>
+										<input type="text" class="form-control" inputClass name="your-company" placeholder="Your Company name">
+									</div>
+									<!-- .col-md-6 end -->
+									<div class="col-xs-12 col-sm-12 col-md-12 checkboxCollumn">
+										<!-- <select class="form-control">
+											<option>Practice Area</option>
+											<option>Criminal Law</option>
+											<option>International Law</option>
+											<option>Financial Law</option>
+										</select> -->
+										<div class="form-check form-check-inline">
+											<label class="form-label">Interested in</label>		
+										</div>
+										<div class="form-check form-check-inline"><!-- checked -->
+										  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" name="interested[]" value="Product Sourcing">
+										  <!-- <span class="checkmark"></span> -->
+										  <label class="form-check-label" for="inlineCheckbox1">Product Sourcing</label>
+										</div>
+										<div class="form-check form-check-inline">
+										  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" name="interested[]" value="China Fulfilment">
+										  <!-- <span class="checkmark"></span> -->
+										  <label class="form-check-label" for="inlineCheckbox2">China Fulfilment</label>
+										</div>
+									</div>
+									<!-- .col-md-12 end -->
+									<div class="col-xs-12 col-sm-12 col-md-12">
+										<label class="form-label">Message</label>
+										<textarea class="form-control inputClass" name="your-message" rows="3" placeholder="Enter your message here"></textarea>
+										<div class="requiredMsg">*Please fill in the required field</div>
+									</div>
+									<!-- .col-md-12 end -->
+									<!-- <div id="recaptcha" class="g-recaptcha"
+								          data-sitekey="6LeeoBIpAAAAAPIeXetei03AESYpk_xDSgqrIXYD"
+								          data-callback="onSubmitContact"
+								          data-size="invisible">			
+								  	</div> -->
+									<div class="col-xs-12 col-sm-12 col-md-6">
+										<input type="checkbox" name="contact_me_by_fax_only" value="1" id="checkFax" autocomplete="off">
+										<input type="hidden" name="formtype" value="contact">
+										<input  id="submit" class="button" name="submit" type="submit" value="Submit">
+									</div>
+									<!-- <script>onloadsContact();</script> -->
+									<!-- .col-md-6 end -->
+								</form>
+							</div>
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-12 col-md-4 pr-0 pl-0 pr-20 pl-60 contactSection">
+						<div class="heading heading-1 wow fadeInUp" data-wow-duration="1s">
+							<h2 class="heading--title white-light-heading-footer mb-60">Contact Information</h2>
+						</div>
+						<div class="contact-content">
+							<div class="contact--icon pb-50">
+								<img src="assets/img/logo-contact-information.png" alt="icon contact information">
+							</div>
+							<ul>
+								<li>
+									<div class="head">Hong Kong Headquarter</div>
+									<p class="pb-10">Room 602-3, 6/F Hua Qin International Building, 
+									350 Queen’s Road Central, Sheung Wan, Hong Kong</p> 
+									
+									<div class="head">China Warehouse</div>
+									<p class="pb-0 mb-15">Building 3, No. 5, Second Ring Road, Gaozan 
+									Industrial Zone, Gaozan Village, Xingtan Town, 
+									Shunde District, Foshan City, Guangdong Province</p>
+								</li>
+								<li>
+									<div class="head">Business Inquiry</div>  
+									<p class="pb-0 mb-30">+852 2186 7961 <br>
+										<a href="https://wa.me/8617725603096" target="_blank">
+										+86 177 2560 3096 (WhatsApp)</a><br>
+										<a href="mailto:info@spslogistics.io" target="_blank">info@spslogistics.io</a>
+									</p>
+								</li>
+								<li>
+									<div class="head">Service Hours</div> 
+									<p class="pb-0 mb-10">Monday – Sunday 9am to 6pm</p>
+								</li>
+								<li>
+									<div class="head">Find Us on Social Media</div>
+									<div class="shareSMDesktop desktopView">
+										<a href="https://instagram.com/spslogistics.io?igshid=YmMyMTA2M2Y=">
+										<img src="assets/img/icon-s1.png" alt="icon social media 1">
+										</a>
+										<a href="https://www.facebook.com/SPS.Logistics.hongkong">
+										<img src="assets/img/icon-s2.png" alt="icon social media 2">
+										</a>
+										<a href="https://www.tiktok.com/@spslogistics">
+										<img src="assets/img/icon-s3.png" alt="icon social media 3">
+										</a>
+									</div>
+									<div class="shareSMMobile mobileView">
+										<a href="https://instagram.com/spslogistics.io?igshid=YmMyMTA2M2Y=">
+										<img src="assets/img/icon-s1-mobile.png" alt="icon social media 1">
+										</a>
+										<a href="https://www.facebook.com/SPS.Logistics.hongkong">
+										<img src="assets/img/icon-s2-mobile.png" alt="icon social media 2">
+										</a>
+										<a href="https://www.tiktok.com/@spslogistics">
+										<img src="assets/img/icon-s3-mobile.png" alt="icon social media 3">
+										</a>
+									</div>
+								</li>
+							</ul>
+							<div class="addInfo">
+								<a href="terms-and-condition.php">Terms and Condition</a>   |   <a href="privacy-policy.php">Privacy Policy</a>
+							</div>
+						</div>
+					</div>
+					<!-- .col-md-6 end-->
+				</div>
+				<!-- .row end -->
+			</div>
+			<!-- .container end -->
+		</div><!-- .footer-widget end -->
+		<!-- Copyrights
+		============================================= -->
+		<div class="footer--copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12 text--center">
+						<span>&copy; SPS Logistics 2023, All Rights Reserved.</span>
+					</div>
+				</div>
+			</div>
+			<!-- .container end -->
+		</div>
+	</footer>
+</div>
+<!-- #wrapper end -->
+
+<!-- Footer Scripts
+============================================= -->
+<script src="assets/js/jquery-2.2.4.min.js"></script>
+<script src="assets/js/plugins.js"></script>
+<script src="assets/js/functions.js"></script>
+
+</body>
+</html>
